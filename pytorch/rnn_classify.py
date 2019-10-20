218b66bb27cd21e67adea2f80ad0edaa76200435
kobai <vkobayashi106@gmail.com>
2019-10-19 22:37:48 -0400

    Seems like train function works

--- pytorch/rnn_classify.py
+++ pytorch/rnn_classify.py
@@ -40,12 +40,14 @@ def read_file(filename):
 #%%
 X = []
 y = []
+cat_dict = {}
 
-for filename in glob.glob('pytorch/data/names/*.txt'):
+for idx,filename in enumerate(glob.glob('pytorch/data/names/*.txt')):
   category = os.path.splitext(os.path.basename(filename))[0]
   X_category = read_file(filename)
+  cat_dict[str(idx)] = category
   X += X_category
-  y += [category] * len(X_category)
+  y += [idx] * len(X_category)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
@@ -61,7 +63,69 @@ def letter_to_tensor(c):
 def name_to_tensor(s):
   tensor = torch.zeros(len(s), 1, n_letters)
   for idx,letter in enumerate(s):
-    tensor[li][0][letter_to_index(letter)] = 1
+    tensor[idx][0][letter_to_index(letter)] = 1
   return tensor
+
+#%%
+class RNN(nn.Module):
+  def __init__(self, input_size, hidden_size, output_size):
+    super(RNN, self).__init__()
+    self.hidden_size = hidden_size
+    self.i2h = nn.Linear(input_size+hidden_size, hidden_size)
+    self.i2o = nn.Linear(input_size+hidden_size, output_size)
+    self.softmax = nn.LogSoftmax(dim=1)
+  
+  def forward(self, input, hidden):
+    combined = torch.cat((input, hidden), 1)
+    hidden = self.i2h(combined)
+    output = self.i2o(combined)
+    output = self.softmax(output)
+    return output, hidden
+  
+  def initHidden(self):
+    return torch.zeros(1, self.hidden_size)
+  
+n_hidden = 128
+rnn = RNN(n_letters, n_hidden, 18)
+
+#%%
+criterion = nn.NLLLoss()
+lr = 0.005
+
+def step(X,y):
+  hidden = rnn.initHidden()
+  rnn.zero_grad()
+
+  name_tensor = name_to_tensor(X)
+  category_tensor = torch.tensor([y], dtype=torch.long)
+  
+  for i in range(name_tensor.size()[0]):
+    output, hidden = rnn(name_tensor[i], hidden)
+  
+  loss = criterion(output, category_tensor)
+  loss.backward()
+
+  for p in rnn.parameters():
+    p.data.add_(-lr, p.grad.data)
+
+  return output, loss.item()
+
+#%%
+def get_guess(tensor):
+  top_n, top_i = output.topk(1)
+  category_i = top_i[0].item()
+  return cat_dict[str(category_i)], category_i
+
+def train():
+  current_loss = 0
+  for idx, (X,y) in enumerate(zip(X_train, y_train)):
+    output, loss = step(X,y)
+    current_loss += loss
+    if idx % 800 == 0:
+      guess, guess_i = get_guess(output) 
+      print(f'{guess} / {cat_dict[str(y)]} Loss: {current_loss/800}')
+      current_loss = 0
+
+train()
 
 #%%
